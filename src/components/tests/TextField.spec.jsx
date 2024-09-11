8c8bf971edf2b528c00a8094f94e39be9c73babc
bm4656 <newjin46@gmail.com>
2024-09-11 21:46:09 +0900

    test: TexField 단위 테스트 마무리

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-// my-class란 class가 항상 적용된 컴포넌트를 렌더링
-beforeEach(async () => {
-  await render(<TextField className="my-class" />);
-});
+// // my-class란 class가 항상 적용된 컴포넌트를 렌더링
+// beforeEach(async () => {
+//   await render(<TextField className="my-class" />);
+// });
 
 it('className prop으로 설정한 css class가 적용된다.', async () => {
   await render(<TextField className="my-class" />);
@@ -32,5 +32,61 @@ describe('placeholder', () => {
     const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
 
     expect(textInput).toBeInTheDocument();
+  });
+});
+
+it('텍스트를 입력하면 onChange prop으로 등록된 함수가 호출된다.', async () => {
+  const spy = vi.fn(); // 스파이 함수
+  // 스파이 함수: 테스트 코드에서 특정 함수가 호출되었는지, 함수의 인자로 어떤 것이 넘어왔는지 어떤 값을 반환하는지 등 다양한 값들을 저장
+  //  콜백 함수나 이벤트 핸들러가 올바르게 호출되었는지 검증하고 싶을 때 사용
+  const { user } = await render(<TextField onChange={spy} />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  await user.type(textInput, 'test');
+
+  expect(spy).toHaveBeenCalledWith('test');
+});
+
+it('엔터키를 입력하면 onEnter prop으로 등록된 함수가 호출된다.', async () => {
+  const spy = vi.fn();
+
+  const { user } = await render(<TextField onEnter={spy} />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  // {Enter} -> type API에서 엔터키 입력 이벤트 발생(다른 키도 작성 가능)
+  await user.type(textInput, 'test{Enter}');
+
+  expect(spy).toHaveBeenCalledWith('test');
+});
+
+it('포커스가 활성화되면 onFocus prop으로 등록된 함수가 호출된다.', async () => {
+  // 포커스 활성화
+  // 탭 키로 인풋 요소로 포커스 이동
+  // 인풋 요소를 클릭했을떄 -> 가장 사용자 보편적이라 생각해서 이걸로 작성해본다.
+  // textInput.focus()로 직접 발생
+  const spy = vi.fn();
+
+  const { user } = await render(<TextField onFocus={spy} />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  await user.click(textInput);
+
+  // 컴포넌트에서 onFocus 호출시 인자를 넘겨주지 않기 때문에 스파이함수의 호출여부만 단언하는 매체 사용
+  expect(spy).toHaveBeenCalled();
+});
+
+it('포커스가 활성화되면 border 스타일이 추가된다.', async () => {
+  const { user } = await render(<TextField />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  await user.click(textInput);
+  //DOM에서 CSS클래스가 아닌 style 속성을 검증하기 위해서는 toHaveStyle을 사용해 단언
+  expect(textInput).toHaveStyle({
+    borderWidth: '2px',
+    borderColor: 'rgb(25, 118, 210)',
   });
 });
--- src/utils/test/setupTests.js
+++ src/utils/test/setupTests.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 
+// 모킹한 모듈의 히스토리를 초기화 -> 다른 테스트에 영향끼치지 않도록 -> 테스트의 독립성 보장
 afterEach(() => {
   vi.clearAllMocks();
 });
